f874c986b98fb62cd1ad14d4b189cb356cf580c0
dpodd <fuzzyworld15@gmail.com>
2021-03-24 14:25:53 +0700

    Add models to main app

--- marketplace/apps/main/models.py
+++ marketplace/apps/main/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Goods(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    seller = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='goods_sell', on_delete=models.CASCADE, null=True)
+    customer = models.ForeignKey(Customer, related_name='goods_buy', on_delete=models.SET_NULL,
+                                 null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title[:30]
+
+    @property
+    def is_ordered(self):
+        return self.customer is not None
