a3e5347d90d886928fd0d17e0316de5c366f2c4b
dpodd <fuzzyworld15@gmail.com>
2021-03-30 08:21:56 +0700

    Pass a couple of variables to a template

--- marketplace/config/urls.py
+++ marketplace/config/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', include('apps.main.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('pages/', include('django.contrib.flatpages.urls')),
 ]
