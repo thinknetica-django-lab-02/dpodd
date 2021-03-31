f11538cdf44c13be2d26731819955ea5a49ea6bb
dpodd <fuzzyworld15@gmail.com>
2021-03-31 09:56:25 +0700

    Add sidebar with Tag list

--- marketplace/apps/main/views.py
+++ marketplace/apps/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View, ListView, DetailView
 
-from .models import Goods
+from .models import Goods, Tag
 
 
 class IndexView(View):
@@ -14,6 +14,12 @@ class GoodsItemsList(ListView):
     model = Goods
     paginate_by = 10
     template_name = 'main/goods_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags_list'] = Tag.objects.all()
+
+        return context
 
 
 class GoodsDetailView(DetailView):
