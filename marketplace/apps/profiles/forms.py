39440dfbc6af1d15b0500c6b260762946e0613a3
dpodd <fuzzyworld15@gmail.com>
2021-04-01 10:29:24 +0700

    Remake Profile model; add OneToOne field to User model

--- marketplace/apps/profiles/forms.py
+++ marketplace/apps/profiles/forms.py
@@ -6,4 +6,4 @@ from .models import Profile
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['first_name', 'last_name', 'email']
+        fields = '__all__'
--- marketplace/apps/profiles/models.py
+++ marketplace/apps/profiles/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Profile(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
