40ab7e66c01098e51ce11a2a35b5e78b115ebe9b
dpodd <fuzzyworld15@gmail.com>
2021-04-01 10:01:35 +0700

    Fix form logic

--- marketplace/apps/profiles/views.py
+++ marketplace/apps/profiles/views.py
@@ -11,3 +11,9 @@ class ProfileView(UpdateView):
 
     def get_success_url(self):
         return reverse('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.method == 'GET':
+            context['profile_pk'] = self.kwargs['pk']
+        return context
