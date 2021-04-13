723d8dadae032aa837891aae8f3f7339efbdc12a
dpodd <fuzzyworld15@gmail.com>
2021-04-13 14:49:28 +0700

    Add permissions check to views, responsible for adding and editing items of Goods

--- marketplace/apps/main/urls.py
+++ marketplace/apps/main/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('goods/', views.GoodsItemsList.as_view(), name='goods-list'),
     path('goods/<int:pk>/', views.GoodsDetailView.as_view(), name='goods-detail'),
     path('goods/<int:pk>/edit', views.EditItemOfGoodsView.as_view(), name='goods-edit'),
-    path('goods/add', views.AddItemOfGoodsView.as_view(), name='goods-add'),
+    path('goods/add/', views.AddItemOfGoodsView.as_view(), name='goods-add'),
 ]
