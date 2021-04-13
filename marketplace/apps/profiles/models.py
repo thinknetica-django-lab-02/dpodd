89bbeacf39fda0b09d06970fe6a891ecd362c558
dpodd <fuzzyworld15@gmail.com>
2021-04-13 12:12:04 +0700

    Add signal to add newly created users to "common users" group; add signal to create Profile for a new user

--- marketplace/apps/profiles/models.py
+++ marketplace/apps/profiles/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from sorl.thumbnail import ImageField
 
@@ -10,3 +13,17 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     birthday = models.DateField(null=True)
     avatar = ImageField(upload_to='avatars', null=True)
+
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    """Creates a Profile object for a new user."""
+    profile, created = Profile.objects.get_or_create(user=instance)
+    instance.profile.save()
+
+
+@receiver(post_save, sender=User)
+def add_created_user_to_group(sender, instance, created, **kwargs):
+    """Adds a newly created user to a "Common Users" group."""
+    if created:
+        instance.groups.add(Group.objects.get(name='common users'))
