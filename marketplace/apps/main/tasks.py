1d9c439d68b61da54e5338a47e50a8b34e325508
dpodd <fuzzyworld15@gmail.com>
2021-04-20 15:21:28 +0700

    Add periodic task to celery (sending notifications on the week additions)

--- marketplace/apps/main/tasks.py
+++ marketplace/apps/main/tasks.py
@@ -1,7 +1,10 @@
+from datetime import datetime, timedelta
+
 from django.core.mail import send_mail
 from django.conf import settings
 
 from config import celery_app
+from apps.main.models import Goods, Subscriber
 
 
 @celery_app.task()
@@ -15,3 +18,28 @@ def notify_new_item_added(email_title: str, to_email: str):
         fail_silently=True
     )
 
+
+@celery_app.task()
+def notify_subscribers_week_additions():
+    """Send an email to subscribers about new goods additions in the last week."""
+
+    week_ago = datetime.utcnow().replace(tzinfo=UTC) - timedelta(days=7)
+    new_goods_last7days = Goods.objects.filter(created_on__gte=week_ago)
+    if new_goods_last7days.exists():
+        message = "We have new additions in our collection!\nJust see:\n" + \
+              '\n'.join([item.title for item in new_goods_last7days])
+    else:
+        message = ''
+    subscribers = Subscriber.objects.select_related('user').all()
+
+    # send mail
+    if message:
+        for subscriber in subscribers:
+            if subscriber.user.email:
+                send_mail(
+                    "[Marketplace] NEW additions in the last week",
+                    message,
+                    settings.EMAIL_DEFAULT_FROM,
+                    [subscriber.user.email],
+                    fail_silently=True
+                )
